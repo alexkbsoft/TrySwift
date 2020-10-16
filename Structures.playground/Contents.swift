05ef470fb23a0d6d99de2765c6cab0040ba0ea88
a.golovanov <san.ok@bk.ru>
2020-10-16 10:35:07 +0700

    structures

---
+++ Structures.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+struct Site {
+    var siteName = "swiftbook.ru"
+    let visitsToday = 1000
+    let visitsYesterday = 1000
+    
+    var visitsTomorrow: Int {
+        return (visitsToday + visitsYesterday)/2
+    }
+    
+    lazy var someLazyVar: Int = 1 + 2
+    
+    func description() -> String {
+        return """
+        We have \(visitsToday); Yesterday it was \(visitsYesterday);
+        \n Estimated tomorrow: \(visitsTomorrow)
+        """
+    }
+}
+
+let firstSite = Site()
+firstSite.description()
+
+func changeSite(site: Site) -> Site {
+    var site = site
+    site.siteName = "swift.ru"
+    return site
+}
+
+changeSite(site: firstSite)
+firstSite
+
+//===
+// let secondWebSite = Site()
+// if firstSite === secondWebSite { // false
+//  print("ecuals")
+// }
+
